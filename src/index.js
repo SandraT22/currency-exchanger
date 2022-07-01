1cbc44a729636621b9f0d87a9d5fac20de9693cb
Sandra Tena <sandratena06@gmail.com>
2022-07-01 13:59:59 -0700

    add error message for selection other than 5 listed currencies

--- src/index.js
+++ src/index.js
@@ -18,19 +18,21 @@ function getElements(response, usdInput, currency) {
   let rates = response.conversion_rates;
   if (currency === "EUR") {
     exchange = rates.EUR * usdInput;
-    $('.exchange').text(`The amout in ${currency} is ${exchange}`);
+    $('.exchange').text(`The amout in ${currency} is $${exchange}`);
   } else if (currency === "CAD") {
     exchange = rates.CAD * usdInput;
-    $('.exchange').text(`The amout in ${currency} is ${exchange}`);
+    $('.exchange').text(`The amout in ${currency} is $${exchange}`);
   } else if (currency === "GBP") {
     exchange = rates.GBP * usdInput;
-    $('.exchange').text(`The amout in ${currency} is ${exchange}`);
+    $('.exchange').text(`The amout in ${currency} is $${exchange}`);
   } else if (currency === "AUD") {
     exchange = rates.AUD * usdInput;
-    $('.exchange').text(`The amout in ${currency} is ${exchange}`);
+    $('.exchange').text(`The amout in ${currency} is $${exchange}`);
   } else if (currency === "JPY") {
     exchange = rates.JPY * usdInput;
-    $('.exchange').text(`The amout in ${currency} is ${exchange}`);
+    $('.exchange').text(`The amout in ${currency} is $${exchange}`);
+  } else {
+    return $('.show-errors').test("Currency selected is not included");
   }
 }
 
