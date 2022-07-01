204310aaf7b1def0beb242e82a339d558f855f38
Sandra Tena <sandratena06@gmail.com>
2022-07-01 10:41:17 -0700

    add UI logic and UI service logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from '../exchange-service,js';
+
+function clearFields() {
+  $('#usdInput').val("");
+  $('.show-errors').text("");
+}
+
+function displayErrors(error) {
+  $('.show-errors').text(`${error}`);
+}
+
+function displayExchange(currency) {
+  let Rates = response.conversion_rates;
+  let exchange = 0;
+  console.log(responseArray);
+  if (currency === "Euro") {
+    exchange = responseArray.EUR * usdInput
+    $('.exchange').text(`The amout in ${currency} is ${exchange}`);
+  }
+}
 
 $(document).ready(function () {
   $('selector').submit(function (event)) {
-    event.preventDefault(); //if it's a form
-    // UI code here
+    event.preventDefault(); 
+    const usdInput = $('#usdInput').val();
+    const currency = $('select#currency').val();
+    Exchange.getExchange(usdInput)
+      .then(function(exchangeResponse) {
+        if (exchangeResponse instanceof Error) {
+          throw Error(`Currency exchange error: ${exchangeResponse.message}`);
+        }
+        displayExchange(currency);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
   }
 });
